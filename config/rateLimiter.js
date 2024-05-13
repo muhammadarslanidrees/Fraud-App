d37e1ba9497c0f77f13f9f366284dfd17d1ccf35
javaidaqib <aqibjavaid.ahmed@systemsltd.com>
2024-05-13 12:28:58 +0500

    feature: add the rate limiting configurations and use it as a middleware in the app

---
+++ config/rateLimiter.js
@@ -0,0 +1,14 @@
+import { rateLimit } from "express-rate-limit";
+
+export const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 60, // How long to remember requests for, in milliseconds (15 minutes)
+  limit: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7",
+  message: {
+    status: 429,
+    message:
+      "You have reached your maximum limit. Please try again after 15 minutes",
+  },
+  statusCode: 429,
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
