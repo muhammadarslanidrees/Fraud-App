2d2d1d05dad9b5f2edb0b8a37a3007eab215b539
Muhammad Arslan <32155657+muhammadarslanidrees@users.noreply.github.com>
2024-12-07 14:15:52 +0500

    Node New App

--- config/filesystem.js
+++
@@ -1,8 +0,0 @@
-export const supportedMimeTypes = [
-  "image/jgp",
-  "image/jpeg",
-  "image/png",
-  "image/svg",
-  "image/gif",
-  "image/webp",
-];
--- routes/router.js
+++ routes/router.js
@@ -1,18 +1,11 @@
 import { Router } from "express";
 
-import AuthRouter from "./auth.routes.js";
-import ProfileRouter from "./profile.routes.js";
-import NewsRouter from "./news.routes.js";
+// import NewsRouter from "./news.routes.js";
 
 const router = Router();
 
-// Auth Routes
-router.use("/auth", AuthRouter);
-
-// Profile Routes
-router.use("/profile", ProfileRouter);
 
 // News Routes
-router.use("/news", NewsRouter);
+// router.use("/news", NewsRouter);
 
 export default router;
